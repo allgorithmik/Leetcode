bdc9897425a612dfe4bcb6f53342a041ac861759
allgorithmik <vvishwas2@gmail.com>
2022-01-09 13:14:36 +0530

    Reverse a string -- Leetcode

---
+++ ReverseString.py
@@ -0,0 +1,7 @@
+def reverseString(inString: str) -> list:
+    listStr = list(inString)
+    revStr = listStr[::-1]
+    print("Input string list: ", listStr)
+    return "Output string list: ", revStr
+
+reverseString() # Input a sting value
